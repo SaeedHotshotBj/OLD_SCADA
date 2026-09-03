784432ca9d754012165ef675ad3b11aef351b145
SaeedHotshotBj <saeedhotshotbj@gmail.com>
2026-09-03 08:02:03 +0330

    Point Edge to server gateway port

--- EDGE/config.py
+++ EDGE/config.py
@@ -7,20 +7,18 @@ PLC_IP = "192.168.1.10"
 PLC_PORT = 502
 SLAVE_ID = 1
 
-# Public URL of the OLD_SCADA server
-SERVER_URL = "http://77.104.95.230"
+# Public URL of the OLD_SCADA Edge gateway
+SERVER_URL = "http://77.104.95.230:5001"
 
-# Edge identity/token. Change this per customer installation.
+# Must match SCADA_EDGE_TOKEN on the server.
 EDGE_TOKEN = "CHANGE_ME"
 
-# PLC read range. The current OLD_SCADA project reads holding registers 0..60.
+# Current OLD_SCADA register range
 READ_START = 0
 READ_COUNT = 61
 
-# Polling intervals (seconds)
 READ_INTERVAL = 0.5
 COMMAND_INTERVAL = 0.5
 HEARTBEAT_INTERVAL = 5
-
 REQUEST_TIMEOUT = 5
 RETRY_DELAY = 2
